179373015a55a900fbf5d6d702346929bf3906c4
Michael <m_j_39@hotmail.com>
2025-02-10 01:56:21 +0100

    Linked list was not commited before?

--- assignment1/src/main/java/dk/dtu/compute/course02324/assignment1/integerstack/implementations/LinkedListStack.java
+++ assignment1/src/main/java/dk/dtu/compute/course02324/assignment1/integerstack/implementations/LinkedListStack.java
@@ -5,62 +5,71 @@ import dk.dtu.compute.course02324.assignment1.integerstack.types.Stack;
 /**
  * Implements a {@link Stack} of integers as a (singly) linked list.
  * TODO note that this is template only, and the actual functions of
- *      the stack are not implemented yet. Impelemnting these methods
+ *      the stack are not implemented yet. Implementing these methods
  *      is the task of assignment 1.
  *
  * @author Ekkart Kindler, ekki@dtu.dk
  */
 public class LinkedListStack implements Stack {
 
-    private StackElement top = null;
+	private StackElement top = null;
 
-    private int size = 0;
+	private int size = 0;
 
-    @Override
-    public void clear() {
-        // TODO must be implemented
-    }
+	@Override
+	public void clear() {
+		top = null;
+		size = 0;
+	}
 
-    @Override
-    public Integer pop() {
+	@Override
+	public Integer pop() {
+		if(top != null) {
+			StackElement element = top;
+			top = element.next;
+			size--;
+			return element.value;
+		}
+		return null;
+	}
 
-        return 0;
-    }
+	@Override
+	public Integer top() {
+		if (top != null) {
+			return top.value;
+		}
+		return null;
+	}
 
-    @Override
-    public Integer top() {
-        // TODO must be implemented
-        return 0;
-    }
+	@Override
+	public void push(Integer value) {
+		StackElement newElement = new StackElement(value, top);
+		size++;
+		top = newElement;
+	}
 
-    @Override
-    public void push(Integer value) {
-        // TODO must be implemented
-    }
-
-    @Override
-    public int size() {
-        // TODO must be implemented
-        return -1;
-    }
+	@Override
+	public int size() {
+		return size;
+	}
 
 
-    /**
-     * Represents a single element on the linked list stack with its
-     * value and a pointer to the next element.
-     *
-     * @author Ekkart Kindler, ekki@dtu.dk
-     */
-    private class StackElement {
+	/**
+	 * Represents a single element on the linked list stack with its
+	 * value and a pointer to the next element.
+	 *
+	 * @author Ekkart Kindler, ekki@dtu.dk
+	 */
+	private class StackElement {
 
-        private Integer value;
+		private Integer value;
 
-        private StackElement next;
+		private StackElement next;
 
-        private StackElement(Integer value, StackElement next) {
-            this.value = value;
-            this.next = next;
-        }
-    }
+		private StackElement(Integer value, StackElement next) {
+			this.value = value;
+			this.next = next;
+		}
+	}
 
 }
