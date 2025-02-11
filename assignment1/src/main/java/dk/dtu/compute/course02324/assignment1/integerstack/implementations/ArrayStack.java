4dcebce91d465a3d2b26bd850c3a37ded7de938e
Ruchir <158055588+Ruchir-r@users.noreply.github.com>
2025-02-11 10:51:05 +0100

    Ruchir 1st Try

--- assignment1/src/main/java/dk/dtu/compute/course02324/assignment1/integerstack/implementations/ArrayStack.java
+++ assignment1/src/main/java/dk/dtu/compute/course02324/assignment1/integerstack/implementations/ArrayStack.java
@@ -1,7 +1,7 @@
 package dk.dtu.compute.course02324.assignment1.integerstack.implementations;
 
 import dk.dtu.compute.course02324.assignment1.integerstack.types.Stack;
-import java.util.Arrays;
+
 /**
  * Implements a {@link Stack} of integers by using a sufficiently large
  * array. Note that the array needs to be dynamically extended by
@@ -9,72 +9,90 @@ import java.util.Arrays;
  * current length of the array.
  *
  * TODO note that this is template only, and the actual functions of
- *       the stack are not implemented yet. Impelenting these methods
+ *       the stack are not implemented yet. Implementing these methods
  *       is the task of assignment 1.
  *
  * @author Ekkart Kindler, ekki@dtu.dk
  */
 public class ArrayStack implements Stack {
 
-	/**
-	 * Constant defining the default size of the array when the
-	 * stack is created. The value can be any (strictly) positive
-	 * number. Here, we have chosen <code>10</code>, which is also
-	 * Java's default for some array-based collection implementations.
-	 */
-	final private int DEFAULT_SIZE = 10;
+    /**
+     * Constant defining the default size of the array when the
+     * stack is created. The value can be any (strictly) positive
+     * number. Here, we have chosen <code>10</code>, which is also
+     * Java's default for some array-based collection implementations.
+     */
 
-	/**
-	 * The array in which the elements of the stack are stored.
-	 * The top of the stack is towards the higher numbers.
-	 *
-	 * TODO note that arrays in Java have fixed size! So the
-	 *      this array will need to be changed at runtime,
-	 *      when need should be!
-	 */
-	private Integer[] array = new Integer[DEFAULT_SIZE];
+    final private int DEFAULT_SIZE = 10;
 
-	/**
-	 * The current size of the stack. Note that the top-level
-	 * element should be located at <code>size-1</code> in this array.
-	 */
-	private int size = 0;
+    /**
+     * The array in which the elements of the stack are stored.
+     * The top of the stack is towards the higher numbers.
+     *
+     * TODO note that arrays in Java have fixed size! So the
+     *      this array will need to be changed at runtime,
+     *      when need should be!
+     */
+    private Integer[] array = new Integer[DEFAULT_SIZE];
 
-	@Override
-	public void clear() {
-		for(int i = 0; i < array.length; i++) {
-			array[i] = null;
-		}
-	}
+    /**
+     * The current size of the stack. Note that the top-level
+     * element should be located at <code>size-1</code> in this array.
+     */
+    private int size = 0;
 
-	@Override
-	public Integer pop() {
-		if (size == 0) return null;
-		Integer topValue = array[size - 1];
-		array[size - 1] = null;
-		size--;
-		return topValue;
-	}
+    @Override
+    public void clear() {
+        // TODO must be implemented
+        array = new Integer[DEFAULT_SIZE];
+        size = 0;
+    }
 
-	@Override
-	public Integer top() {
-		if (size == 0) {
-			return null;
-		} else {
-			return array[size - 1];
-		}
-	}
+    @Override
+    public Integer pop() {
+        // TODO must be implemented
+        if (size!=0) {
+            int element = array[size - 1];
+            array[size - 1] = null;
+            size--;
+            if (size < array.length/2 && size >= DEFAULT_SIZE-1) {
+                Integer[] newArray = new Integer[array.length/2];
+                for (int i = 0; i < size ; i++) {
+                    newArray[i] = array[i];
+                }
+                // System.arraycopy(array, 0, newArray, 0, size);
+                array = newArray;
+            }
+            return element;
+        }
+        return null;
+    }
 
-	@Override
-	public void push(Integer value) {
-		if (size == array.length) {
-			array = Arrays.copyOf(array, array.length * 2);
-		}
-		array[size++] = value;
-	}
+    @Override
+    public Integer top() {
+        // TODO must be implemented
+        if (size!=0){
+            return array[size-1];
+        }
+        return null;
+    }
 
-	@Override
-	public int size() {
-		return size;
-	}
+    @Override
+    public void push(Integer value) {
+        // TODO must be implemented
+        if (size==array.length){
+            Integer[] newArray = new Integer[size * 2];
+            System.arraycopy(array, 0, newArray, 0, size);
+            array = newArray;
+        }
+        array[size] = value;
+        size++;
+    }
+
+    @Override
+    public int size() {
+        // TODO must be implemented
+        return size;
+    }
+
 }
